285a89c995989712f71d185ccb0e1fd0e2dce5a1
Médi-Rémi Hashim <mediremi@gmail.com>
2020-09-03 16:35:49 +0100

    Evaluate model

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -5,6 +7,29 @@ import torch.optim as optim
 
 import torchvision
 from torchvision import transforms, datasets
+
+IMAGE_WIDTH = 28
+IMAGE_HEIGHT = 28
+INPUT_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
+LAYER_SIZE = 64
+OUTPUT_SIZE = 10
+MODEL_PATH = "model/model.pt"
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden_layers = nn.ModuleList([
+            nn.Linear(INPUT_SIZE, LAYER_SIZE),
+            nn.Linear(LAYER_SIZE, LAYER_SIZE),
+            nn.Linear(LAYER_SIZE, LAYER_SIZE)
+        ])
+        self.output_layer = nn.Linear(LAYER_SIZE, OUTPUT_SIZE)
+
+    def forward(self, x):
+        for layer in self.hidden_layers:
+            x = F.relu(layer(x))
+        return F.log_softmax(self.output_layer(x), dim=1)
+        return x
 
 def get_dataset(train=False):
     return datasets.MNIST(
@@ -15,49 +40,60 @@ def get_dataset(train=False):
             transforms.ToTensor()
         ]))
 
-BATCH_SIZE = 10
-def load_data(dataset, shuffle=False):
-    return torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=shuffle)
+def load_data(dataset, shuffle=False, batch_size=10):
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle)
 
-train = get_dataset(train=True)
-test = get_dataset()
+def train(model, trainset, epochs=3):
+    model.train()
+    for epoch in range(epochs):
+        for data in trainset:
+            X, y = data
+            model.zero_grad()
+            output = model(X.view(-1, INPUT_SIZE))
+            loss = F.nll_loss(output, y)
+            loss.backward()
+            optimiser.step()
+        print(loss)
 
-trainset = load_data(train, shuffle=True)
-testset = load_data(test)
+def evaluate(model, testset):
+    correct = 0
+    total = 0
+    model.eval()
+    with torch.no_grad():
+        for data in testset:
+            X, y = data
+            output = model(X.view(-1, INPUT_SIZE))
+            for i, expected in enumerate(output):
+                if torch.argmax(expected) == y[i]:
+                    correct += 1
+                total += 1
+    accuracy = correct / total
+    return accuracy
 
-IMAGE_WIDTH = 28
-IMAGE_HEIGHT = 28
+def save_model(model):
+    torch.save(model.state_dict(), MODEL_PATH)
 
-INPUT_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
-LAYER_SIZE = 64
-OUTPUT_SIZE = 10
+def load_model(model_class):
+    model = model_class()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    return model
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.hidden_layers = [
-            nn.Linear(INPUT_SIZE, LAYER_SIZE),
-            nn.Linear(LAYER_SIZE, LAYER_SIZE),
-            nn.Linear(LAYER_SIZE, LAYER_SIZE)
-        ]
-        self.output_layer = nn.Linear(LAYER_SIZE, OUTPUT_SIZE)
+training = get_dataset(train=True)
+testing = get_dataset()
 
-    def forward(self, x):
-        for layer in self.hidden_layers:
-            x = F.relu(layer(x))
-        return F.softmax(self.output_layer(x), dim=1)
+trainset = load_data(training, shuffle=True)
+testset = load_data(testing)
 
-net = Net()
-optimiser = optim.Adam(net.parameters(), lr=0.001)
+if os.path.isfile(MODEL_PATH):
+    net = load_model(Net)
+else:
+    net = Net()
+    optimiser = optim.Adam(net.parameters(), lr=0.001)
+    train(net, trainset)
+    save_model(net)
 
-EPOCHS = 3
-
-for epoch in range(EPOCHS):
-    for data in trainset:
-        X, y = data
-        net.zero_grad()
-        output = net(X.view(-1, INPUT_SIZE))
-        loss = F.nll_loss(output, y)
-        loss.backward()
-        optimiser.step()
-    print(loss)
+accuracy = evaluate(net, testset)
+print("Testset accuracy: {}%".format(round(accuracy, 3) * 100))
